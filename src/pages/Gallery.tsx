e7799a7a264e3f607e64bb0449e8175798024e86
Alex Gerzon <alex.gerzon@ecentria.com>
2025-04-10 12:12:52 -0400

    Add deliberate error to test deployment

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -4,6 +4,7 @@ import { Box, SimpleGrid, Image, Container, Heading, Spinner, Text, Code, Modal,
 import { useGoogleDriveFolder } from '../hooks/useGoogleDriveFolder';
 
 const Gallery = () => {
+  DELIBERATE_ERROR_HERE;  // This should cause a different error if the changes are being picked up
   const { images, loading, error } = useGoogleDriveFolder();
   const [selectedImage, setSelectedImage] = useState<{ name: string; url: string } | null>(null);
   const { isOpen, onOpen, onClose } = useDisclosure();
