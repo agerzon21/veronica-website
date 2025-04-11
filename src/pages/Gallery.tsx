5a8314bf955f53e0067bba3119804b91effe2509
Alex Gerzon <alex.gerzon@ecentria.com>
2025-04-11 19:49:05 -0400

    fix: Update hero image position to center 40%

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Text, IconButton, Flex, Heading } from '@chakra-ui/react';
+import { Box, Container, Text, IconButton, Flex } from '@chakra-ui/react';
 import { useParams, Link } from 'react-router-dom';
 import { ArrowBackIcon } from '@chakra-ui/icons';
 import GalleryCategories from '../components/GalleryCategories';
@@ -75,7 +75,7 @@ const Gallery = () => {
             bottom={0}
             backgroundImage="url(https://res.cloudinary.com/doj1fanx3/image/upload/v1744310089/IMG_3712_uvjtxr.jpg)"
             backgroundSize="cover"
-            backgroundPosition="center"
+            backgroundPosition="center 45%"
             backgroundRepeat="no-repeat"
             filter="brightness(0.5)"
           />
